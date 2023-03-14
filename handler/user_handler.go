0057da5510b9a7143d9a7476f5e318af70e3341d
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-14 14:42:05 +0700

    web

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -1,10 +1,26 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"golang-fiber/database"
+	"golang-fiber/model/entity"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func UserHandlerRead(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{
 		"data": "user",
 	})
+
+}
+
+func UserHandlerGetAll(ctx *fiber.Ctx) error {
+	var users []entity.User
+	result := database.DB.Find(&users)
+	if result.Error != nil {
+		log.Println(result.Error)
+	}
+	return ctx.JSON(users)
 
 }
